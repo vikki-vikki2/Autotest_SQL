3deb69d126ab9814fafe028cb32d6c5f0d8f4226
vikki-vikki2 <157246801+vikki-vikki2@users.noreply.github.com>
2024-02-18 23:26:31 +0300

    Create configuration.py

---
+++ configuration/configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://91ba1a9a-7a8c-45e9-a200-df61bfe90e95.serverhub.praktikum-services.ru" # адрес сервера
+CREATE_COURIER = "/api/v1/courier"   # создание курьера
+CREATE_ORDER = "/api/v1/orders"  # создание заказа
+ORDER_INFORMATION = "/api/v1/orders/track?t="   # получить информацию о заказе по его номеру
