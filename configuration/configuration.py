7539603d37b86a3fd0bc761e01dc61dc7b270587
vikki-vikki2 <157246801+vikki-vikki2@users.noreply.github.com>
2024-02-18 23:34:56 +0300

    configuration.py

--- configuration/configuration.py
+++ configuration/configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://91ba1a9a-7a8c-45e9-a200-df61bfe90e95.serverhub.praktikum-services.ru" # адрес сервера
+URL_SERVICE = "https://8fe756fe-b870-404b-a26e-1026795c145d.serverhub.praktikum-services.ru" # адрес сервера
 CREATE_COURIER = "/api/v1/courier"   # создание курьера
 CREATE_ORDER = "/api/v1/orders"  # создание заказа
 ORDER_INFORMATION = "/api/v1/orders/track?t="   # получение информации о заказе по номеру
