b25e3e9e3a818843da43d89b066889f97025bbb3
vikki-vikki2 <157246801+vikki-vikki2@users.noreply.github.com>
2024-02-18 23:31:25 +0300

    request.py

---
+++ request.py
@@ -0,0 +1,10 @@
+import configuration
+import requests
+
+# создание заказа c заданным телом
+def create_order(order_body):
+   return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER,
+                         json=order_body)
+
+def get_order_info_by_track(track_number):
+    return requests.get(configuration.URL_SERVICE + configuration.ORDER_INFORMATION + str(track_number))
